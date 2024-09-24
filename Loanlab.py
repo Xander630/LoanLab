3f623170bd4892584bf668f97d77ec558660ec44
Xander630 <xandernoora@gmail.com>
2024-09-24 11:30:15 +0800

    Loanlab.py

--- Loanlab.py
+++ Loanlab.py
@@ -1,3 +1,15 @@
+                            # Problem Analysis
+#Ask User for A name and Salary
+#If Salary is equal or greater than 30000 Display Eligible For loan else Display uneligible for Loan
+#If Eligible Ask for a Loan amount and declare a variable for the 10x of the salary to check if eligible
+#If the Loan is less than the 10x of the Salary proceed and display the interest rate
+#If the Loan is greater than the 10 of the Salary Display The loan has been denied
+# If the loan is less than the 10x of the Salary ask the user on Over how many months does the user plan to repay the loan
+#Calculate the amount that the user has to pay use Simple Interest Formula (Principle*Rate*Time) and add to the original loan
+# Display the Final amount that the user has to pay
+# End the Program
+
+
 Name = input("What is your Name: ")
 Salary = float(input("What is your Current Monthly Salary?: "))
 
